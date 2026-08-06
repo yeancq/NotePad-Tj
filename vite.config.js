6523f371059c873e7a0701f9638ff79038401773
yeancq <92113249+yeancq@users.noreply.github.com>
2026-08-07 00:00:10 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/NotePad-Tj/',
+  base: './',
   plugins: [
     react(),
     VitePWA({
