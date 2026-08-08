7d9fca62ff15845af30b8ff2e814c02ef4870319
yeancq <92113249+yeancq@users.noreply.github.com>
2026-08-09 00:26:11 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/NotePad-Tj/',
   plugins: [
     react(),
     VitePWA({
